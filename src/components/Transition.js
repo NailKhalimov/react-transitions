51217c39936c479569a0f5ab78377bdec2f5e97a
Nail` Khalimov <nail980@mail.ru>
2018-10-29 23:04:01 +0300

    init

---
+++ src/components/Transition.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import Transition from 'react-transition-group/Transition'
+
+import '../css/App.css';
+
+class TransitionComp extends Component{
+
+    state = {
+        show: true
+    }
+
+    showDiv = () => {
+        
+        this.setState({
+            show: !this.state.show ? true : false
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <Transition
+                    in={this.state.show}
+                    timeout={{
+                        enter: 2000,
+                        exit: 2000
+                    
+                    }}
+
+                    enter = {true}
+                    exit = {true}
+
+                    onEnter={ (node) => {
+                        console.log('enter')
+                    }}
+
+                    onExit={(node)=>{
+                        console.log('exit')
+                    }}
+                >
+                    {state =>
+                        <div className={`square square-${state}`}>
+                            {`square square-${state}`}
+                        </div>    
+                    }
+                </Transition>
+                
+                <div className="showDiv"
+                    onClick={this.showDiv}
+                >
+                    show or hide
+                </div>
+            </div>
+
+        )
+    }
+}
+
+
+export default TransitionComp;
